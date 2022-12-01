22a975cdfef188d7d8f5fb3cca5931d000d6e4f6
Mohamed Hassanin <mhassanin@microsoft.com>
2022-12-01 14:49:20 -0500

    add Dummy API

---
+++ web-api/web-api/Controllers/DummyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_api.Controllers.Dummy
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DummyController : ControllerBase
+    {
+        /// <summary>
+        ///  Adds two numbers via a GET HTTP action
+        /// </summary>
+        /// <param name="x"> First operand</param>
+        /// <param name="y"> Second Operand</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        [Route("")]
+        [HttpGet]
+        public async Task<double> Test123([FromQuery(Name = "subscription-key")] string subscriptionKey, [FromQuery(Name = "query")] string query)
+        {
+            Console.WriteLine($"received {subscriptionKey} and {query}");
+            return 42;
+        }
+
+    }
+}
